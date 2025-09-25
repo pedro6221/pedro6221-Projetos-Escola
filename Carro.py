c05ca013c1c2fb2dae4e88af745f24937feaa23c
pedro6221 <jpfa0809@gmail.com>
2025-09-25 08:48:56 -0300

    esqueci de uns detalhes

--- Carro.py
+++ Carro.py
@@ -5,26 +5,44 @@ class Carro:
         self.ano = ano
         self.cor = cor
         self.velocidade = 0
+
     def acelerar(self, valor):
+
         self.velocidade += valor
+
         print(f"{self.modelo} acelerou para {self.velocidade} km/h!")
+
     def frear(self, valor):
+
         self.velocidade -= valor
+
         if self.velocidade < 0:
+
+
             self.velocidade = 0
+
         print(f"{self.modelo} reduziu para {self.velocidade} km/h.")
+
+
     def detalhes(self):
-        return (f"{self.marca} {self.modelo} ({self.ano}) - "
-                f"Cor: {self.cor}, Velocidade: {self.velocidade} km/h")
+
+        return (f"{self.marca} {self.modelo} ({self.ano}) - "  f"Cor: {self.cor}, Velocidade: {self.velocidade} km/h")
     
 carro1 = Carro("Toyota", "Corolla", 2020, "Preto")
 carro2 = Carro("Honda", "Civic", 2019, "Vermelho")
 
 print(carro1.detalhes())
+
 print(carro2.detalhes())
+
 carro1.acelerar(50)
+
 carro2.acelerar(30)
+
 carro1.frear(20)
+
 carro2.frear(15)
+
 print(carro1.detalhes())
+
 print(carro2.detalhes())
